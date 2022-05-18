ffa24d556f19c7e35a125f51650262ae186a4043
Tim Zimmermann <tim@linux4.de>
2022-05-18 17:37:58 +0200

    build: Support building Leaf
    Change-Id: I915291a1f5491b20ee9791b36233fd089d659226

--- core/main.mk
+++ core/main.mk
@@ -488,6 +488,9 @@ ADDITIONAL_SYSTEM_PROPERTIES += net.bt.name=Android
 ADDITIONAL_SYSTEM_PROPERTIES += ro.force.debuggable=0
 
 # ------------------------------------------------------------
+# Include vendor specific additions to build properties
+-include vendor/leaf/build/core/main.mk
+# ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
 # non-empty if that module should be installed in /system.
 
