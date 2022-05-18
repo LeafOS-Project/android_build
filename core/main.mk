c990a622779b734a145c8679709913f47bdbeaa3
Tim Zimmermann <tim@linux4.de>
2022-05-18 17:37:58 +0200

    build: Support building Leaf
    Change-Id: I915291a1f5491b20ee9791b36233fd089d659226

--- core/main.mk
+++ core/main.mk
@@ -210,6 +210,9 @@ endif
 BUILD_WITHOUT_PV := true
 
 # ------------------------------------------------------------
+# Include vendor specific additions to build properties
+-include vendor/leaf/build/core/main.mk
+# ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
 # non-empty if that module should be installed in /system.
 
