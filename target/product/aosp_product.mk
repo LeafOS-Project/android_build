061df4790f01489775959bf5f1753406a0b51f72
Tim Zimmermann <tim@linux4.de>
2022-12-10 22:07:52 +0100

    build: Copy apns-conf.xml from vendor/leaf
    Change-Id: I716eab9248995905ebdf80f2324d55f38ad16e09

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -37,4 +37,4 @@ PRODUCT_PACKAGES += \
 # Telephony:
 #   Provide a APN configuration to GSI product
 PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+    vendor/leaf/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
