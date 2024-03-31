c82244649053766c30ac6a28eb7f4ef1155de712
Tim Zimmermann <tim@linux4.de>
2024-03-31 17:37:57 +0200

    envsetup: Make passing release optional for lunch
    Change-Id: Iaaeeb6ce7b5ad2735c16c4b6a5929415ccdb23a7

--- envsetup.sh
+++ envsetup.sh
@@ -810,6 +810,11 @@ function lunch()
     # Split string on the '-' character.
     IFS="-" read -r product release variant <<< "$selection"
 
+    if [ -z "$variant" ]; then
+        variant=$release
+        release=$(ls -1 -I trunk build/release/aconfig/)
+    fi
+
     if [[ -z "$product" ]] || [[ -z "$release" ]] || [[ -z "$variant" ]]
     then
         echo
